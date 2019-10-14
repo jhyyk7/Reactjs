ca8f079f7450ce5907a5ba792bf7699d58c851ed
jhyyk7 <jhyyk7@gmail.com>
2019-10-15 04:16:17 +0900

    Add component event

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -43,9 +43,13 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={
+      mode:'read',
+      selected_content_id:2,
+      welcome:{title:'Welcome', desc:'Hello,Reacat!!'},
       subject:{title:'WEB', sub:'World Wide Web'},
       toc:[
         {id:1, title:'HTML',desc:'HTML is...'},
+        
         {id:2, title:'CSS',desc:'CSS is...'},
         {id:3, title:'Javascript',desc:'Javascript is...'},
 
@@ -57,21 +61,53 @@ class App extends Component {
   }
   
   render() {
+    var _title,_desc=null;
+    if(this.state.mode==='welcome'){
+      _title=this.state.welcome.title
+      _desc=this.state.welcome.desc
+    }
+    else if(this.state.mode==='read'){         
+      for(var i=0;i<this.state.toc.length;i++){
+        
+        var data=this.state.toc[i];
+        if(data.id===this.state.selected_content_id){
+          _title=data.title;
+          _desc=data.desc;
+          break;
+        }
+       
+      }
+    }
   return (
     <div className="App">
      <Subject 
+     
      title={this.state.subject.title} 
-     sub={this.state.subject.sub}>
+     sub={this.state.subject.sub}
+     onChangePage={function(){
+        
+          this.setState({
+            mode:'welcome'
+          });
+        }.bind(this)}
+     >
      </Subject>
      
      <TOC
+     onChangePage={function(id){
+       
+       this.setState({
+         mode:'read',
+         selected_content_id:Number(id),
+       })
+     }.bind(this)}
      data={this.state.toc}
 
      ></TOC>
 
      <Content 
-     title={this.state.content.title}
-     desc={this.state.content.desc}>       
+     title={_title}
+     desc={_desc}>       
      </Content>
     </div>
   );
